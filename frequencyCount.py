d93622babe3b5f8865c7f230e3b6e38dd3745422
alexVinod <39164427+alexVinod@users.noreply.github.com>
2019-07-31 14:56:30 +0530

    Frequency Count Example
    Frequency Count Example

---
+++ frequencyCount.py
@@ -0,0 +1,4 @@
+def fCount(data):
+    pass
+
+fq="Ground Ground Water Rock Bug Rock Dark Dark Grass Grass Dark Dark Grass Grass Grass Grass Bug Water Bug Bug Water Grass Grass Water Dark Dark Water Bug Dark Dark Bug Bug Bug Bug Bug Rock Bug Grass Bug Bug Grass Dark Dark Grass Rock Grass Fire Fire Normal Fire Grass Dark Dark Grass Fire Dark Dark Fire Electric Dark Dark Electric Ground Dark Dark Ground Ground Grass Grass Ground Ground Grass Grass Ground Ghost Grass Grass Dark Dark Ghost Ghost Flying Normal Ghost Dark Dark Grass Grass Dark Grass Flying Poison Flying Poison Poison Flying Dragon Normal Fairy Fighting Fighting Psychic Psychic Psychic Rock Fairy Rock Rock Steel Steel Steel Steel Ice Ice"
